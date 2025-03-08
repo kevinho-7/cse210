4762899d5b37913f0bd58a2b975b54e342d94b48
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-03-07 21:09:42 -0300

    exercise 3 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -12,10 +12,21 @@ class Program
 
         Console.WriteLine("Find the magic number!!");
         Console.Write("What's your guess? -  "); int guess = int.Parse(Console.ReadLine());
-        
 
-        
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("The magic number is higher");
+            }
+            else
+            {
+                Console.WriteLine("The magic number is lower");
+            }
 
+            Console.Write("What's your guess? -  "); guess = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Congratulations! You found the magic number!");
 
     }
 }
