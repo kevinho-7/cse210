63155eda2f6ab04bf4a3249ac13176529ca2885c
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-03-11 15:53:00 -0300

    changes in the poject

---
+++ week02/Journal/PromptGenarator.cs
@@ -0,0 +1,24 @@
+public class  PromptGenarator
+{
+    public List<string> _journalPrompts = new List<string>
+        {
+            "What made me smile today?",
+            "What was a small moment of gratitude I experienced?",
+            "How was I a positive influence on someone today?",
+            "What challenge did I face, and how did I handle it?",
+            "If I could relive one moment of the day, what would it be and why?",
+            "What did I learn today that I can carry into the future?",
+            "How did I show love or kindness to someone today?",
+            "What was the best decision I made today?",
+            "What could I have done differently to make my day even better?",
+            "How did I feel peace or purpose in my day?"
+        };
+
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_journalPrompts.Count);
+        return _journalPrompts[index];
+    }
+}
