b39cdfca9674b2674e78aecfa489a33ef0f7b51d
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-03-18 18:04:40 -0300

    sips

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,18 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int bottom)
+    {
+        _top = 1;
+        _bottom = bottom;
+    }
+}
