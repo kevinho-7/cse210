515bd7746b805e390dedbb2dfffdcc5e681e1d15
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-04-01 14:36:36 -0300

    1 change

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -9,17 +9,7 @@ public class Assignment
         _studentName = name;
         _topic = topic;
     }
-
-    public void SetStudentName(string name)
-    {
-        _studentName = name;
-    }
-
-    public void SetTopic(string topic)
-    {
-        _topic = topic;
-    }
-
+    
     public string GetSummary()
     {
         return $"{_studentName} - {_topic}";
