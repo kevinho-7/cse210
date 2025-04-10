be1dc44ac8ecaf01eec2706f745ce6f196419a8a
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-04-10 10:49:49 -0300

    allright

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine();
+
+        List<Shapes> shapes = new List<Shapes>();
+
+        Square s1 = new Square("red", 5);
+        s1.SetName("Square");
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("blue", 4, 6);
+        r1.SetName("Rectangle");
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("yellow", 3);
+        c1.SetName("Circle");
+        shapes.Add(c1);
+
+
+        foreach (Shapes shape in shapes)
+        {
+            string name = shape.GetName();
+            string color = shape.GetColor();
+            double area = shape.GetArea();  
+
+            Console.WriteLine($"the {shape.GetName()} has the color {shape.GetColor()} and the area is {shape.GetArea()}");
+        }
     }
 }
