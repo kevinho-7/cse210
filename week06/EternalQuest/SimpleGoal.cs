0e7bdfb4fbbd49b17dc3e18552aec1e4836123ff
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-04-11 16:52:56 -0300

    get started

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, string points) : base(name, description, points)
+    {
+
+    }
+
+    public override void RecordEvent()
+    {
+        
+    }
+}
