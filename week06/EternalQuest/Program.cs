f69b0847136acbe1b4f7378a899ce34c800650ee
kevinho-7 <kevingarciaf.kf@outlook.com>
2025-04-12 16:00:04 -0300

    done!!!

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // For the 'Show creativity', the user is not allowed to type anything different from the options. If they try, an error message will appear.
+
         GoalManager gol = new GoalManager();
         gol.Start();
     }
